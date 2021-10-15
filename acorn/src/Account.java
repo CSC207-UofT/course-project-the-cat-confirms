ebb7050e3172f209b1fe2bb2baa47adde2a92a32
yifan <peteryifan.zhao@mail.utoronto.ca>
2021-10-14 21:24:51 -0400

    peter2

--- acorn/src/Account.java
+++ acorn/src/Account.java
@@ -1,15 +1,13 @@
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class Account {
     private final String utorid;
     private String password;
-    private final List<Object> course;
-    /*private List<T> course;
+    private final List<Course> course;
 
-     */
-
-    private Account(String utorid, String password){
+    Account(String utorid, String password){
         this.utorid = utorid;
         this.password = password;
         this.course = new ArrayList<>();
@@ -27,12 +25,15 @@ public class Account {
         this.password = newPassword;
     }
 
-    public List<Object> getCourse() {
-        return course;
+    public ArrayList<String> getCourse() {
+        ArrayList<String> name = new ArrayList<>();
+        for (Course i : course){
+            name.add(i.getCourseName());
+        }
+        return name;
     }
 
-    public void addToCourse(Object object){this.course.add(object);
-    }
+    public void addToCourse(Course course){this.course.add(course);}
 
     public static void main(String[] args) {
         Account x = new Account("zhaoyi61", "123456"
@@ -40,10 +41,10 @@ public class Account {
         System.out.println(x.getUtorid());
         System.out.println(x.getPassword());
         System.out.println(x.getCourse());
-        x.addToCourse("CSC148");
         System.out.println(x.getCourse());
         x.setPassword("654321");
         System.out.println(x.getPassword());
+        System.out.println(x);
     }
 }
 
--- acorn/src/Course.java
+++ acorn/src/Course.java
@@ -1,2 +1,70 @@
+import org.codehaus.groovy.ast.stmt.ContinueStatement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+    private final String courseName;
+    private final int courseTerm;
+    private final String courseId;
+    private int numberOfAccount;
+    private final List<Account> accountInCourse;
+
+    private Course(String courseName, int courseTerm,
+                   String courseId){
+        this.courseName = courseName;
+        this.courseTerm = courseTerm;
+        this.courseId = courseId;
+        this.numberOfAccount = 0;
+        this.accountInCourse = new ArrayList<>();
+        }
+
+    public int getCourseTerm() {
+        return courseTerm;
+    }
+
+    public ArrayList<String> getAccountInCourse() {
+        ArrayList<String> name = new ArrayList<>();
+        for (Account i : accountInCourse) {
+            name.add(i.getUtorid());
+        }
+        return name;
+    }
+
+    public int getNumberOfAccount() {
+        return numberOfAccount;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void addAccount(Account newStudent){
+        if (accountInCourse.contains(newStudent)) {
+            System.out.println("student already in courses");
+        }
+        else {accountInCourse.add(newStudent);
+        numberOfAccount ++;
+        newStudent.addToCourse(new Course(this.courseName,this.courseTerm,this.courseId));
+        }
+    }
+
+    public static void main(String[] args) {
+        Account studentA = new Account("Peter","123457");
+        Course x = new Course("csc207",3,"1");
+        System.out.println(x.getNumberOfAccount());
+        System.out.println(x.getAccountInCourse());
+        x.addAccount(studentA);
+        System.out.println(x.getAccountInCourse());
+        System.out.println(x.getNumberOfAccount());
+        System.out.println(studentA.getCourse());
+        x.addAccount(studentA);
+        System.out.println(studentA.getCourse());
+
+    }
+
 }
