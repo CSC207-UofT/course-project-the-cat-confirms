de679220d98d60228745b9bdd790e7296b167f76
peter <peter.yifan.zhao@gmail.com>
2021-11-14 16:28:52 -0500

    peter has modify the SystemInOut.java file for a more clear Front development

--- src/main/java/SystemInOut.java
+++ src/main/java/SystemInOut.java
@@ -18,33 +18,54 @@ public class SystemInOut {
     }
 
     public void startInteract(UserProfile userProfile, ChatroomManager chatroomManager) {
-        System.out.println("Please enter a command:");
+        System.out.println("Please enter a command in 'chat':");
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
-            String[] args = line.split(" ");
-            String cmd = args[0];
-            if (cmd.equals("chat")) {
-                String sub_cmd = args[1];
-                if (sub_cmd.equals("new")) {
-                    String roomName = args[2];
+            if (line.equals("chat")) {
+                System.out.println("Please enter a room name");
+                while (scanner.hasNext()) {
+                    String roomName = scanner.nextLine();
+                    System.out.println("creating a new chat room");
                     Chatroom chatroom = new Chatroom(roomName, userProfile.getOwner());
                     chatroomManager.addChatRoom(chatroom);
-                } else {
-                    String roomName = sub_cmd;
-
-                    String msgText = args[2];
-                    TextMessage msg = new TextMessage(msgText, userProfile.getOwner());
-
-                    for (Chatroom chatroom :
-                            chatroomManager.getChatRooms()) {
+                    System.out.println("type what you want");
+                    while (scanner.hasNext()) {
+                        String msgText = scanner.nextLine();
+                        TextMessage msg = new TextMessage(msgText, userProfile.getOwner());
                         chatroom.addMessage(msg);
+                        System.out.println("type what you want");
+                        }
                     }
                 }
             }
-
-
-            System.out.println("Please enter a command:");
         }
-
-    }
 }
+//        System.out.println("Please enter a command:");
+//        while (scanner.hasNext()) {
+//            String line = scanner.nextLine();
+//            String[] args = line.split(" ");
+//            String cmd = args[0];
+//            if (cmd.equals("chat")) {
+//                String sub_cmd = args[1];
+//                if (sub_cmd.equals("new")) {
+//                    String roomName = args[2];
+//                    Chatroom chatroom = new Chatroom(roomName, userProfile.getOwner());
+//                    chatroomManager.addChatRoom(chatroom);
+//                } else {
+//                    String roomName = sub_cmd;
+//
+//                    String msgText = args[2];
+//                    TextMessage msg = new TextMessage(msgText, userProfile.getOwner());
+//
+//                    for (Chatroom chatroom :
+//                            chatroomManager.getChatRooms()) {
+//                        chatroom.addMessage(msg);
+//                    }
+//                }
+//            }
+//
+//
+//            System.out.println("Please enter a command:");
+//        }
+//
+//    }
