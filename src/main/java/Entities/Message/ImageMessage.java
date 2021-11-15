5ec44813845fa862ed04d399c804312f6cd318d9
Varun Pillai <varunsureshpillai@gmail.com>
2021-11-15 15:48:41 -0500

    Implemented ImageMessage to give basic image messaging implementation using file sharing, where the file contains the image.

--- src/main/java/Entities/Message/ImageMessage.java
+++ src/main/java/Entities/Message/ImageMessage.java
@@ -1,5 +1,26 @@
 package Entities.Message;
 
-public class ImageMessage extends Message<String> {
-    // TODO: implement ME
+import Entities.User;
+
+import java.io.File;
+
+public class ImageMessage extends Message<File>{
+    private final File image;
+
+    public ImageMessage(File img, User sender) {
+        this.image = img;
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "ImageMessage{" +
+                "sender='" + this.sender + '\'' +
+                "image='" + this.image + '\'' +
+                '}';
+    }
+
+    public String getImagePath(){
+            return image.getPath();
+        }
 }
