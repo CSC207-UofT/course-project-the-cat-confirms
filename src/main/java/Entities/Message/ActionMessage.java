3161959aa63063e08b4679891d45af9b17832e33
Varun Pillai <varunsureshpillai@gmail.com>
2021-11-15 15:30:03 -0500

    Implemented ActionMessage to give reaction in only {Like, Dislike, :)} to an original message.

--- src/main/java/Entities/Message/ActionMessage.java
+++ src/main/java/Entities/Message/ActionMessage.java
@@ -2,20 +2,31 @@ package Entities.Message;
 
 import Entities.User;
 
+import java.util.ArrayList;
+
 public class ActionMessage extends Message<String> {
     private final String action;
     public final TextMessage original;
 
 
+
     public ActionMessage(String act, User sender, TextMessage original) {
-        this.action = act;
-        this.sender = sender;
-        this.original = original;
+        ArrayList<String> listOfActions = new ArrayList<>();
+        listOfActions.add("Like");
+        listOfActions.add("Dislike");
+        listOfActions.add(":)");
+        if (listOfActions.contains(act)) {
+            this.action = act;
+            this.sender = sender;
+            this.original = original;
+        } else {
+            throw new IllegalArgumentException("action can only be one of {Like, Dislike, :)}");
+        }
     }
 
     @Override
     public String toString() {
-        return "TextMessage{" +
+        return "ActionMessage{" +
                 "sender='" + this.sender + '\'' +
                 "action='" + this.action + '\'' +
                 "to original message='" + this.original + '\'' +
