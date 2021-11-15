195049bb5719d95ce302947095d5858334cf3360
Varun Pillai <varunsureshpillai@gmail.com>
2021-11-15 15:14:51 -0500

    Implemented ActionMessage to give basic reaction to a given message

--- src/main/java/Entities/Message/ActionMessage.java
+++ src/main/java/Entities/Message/ActionMessage.java
@@ -1,5 +1,24 @@
 package Entities.Message;
 
+import Entities.User;
+
 public class ActionMessage extends Message<String> {
-    // TODO: implement ME
+    private final String action;
+    public final TextMessage original;
+
+
+    public ActionMessage(String act, User sender, TextMessage original) {
+        this.action = act;
+        this.sender = sender;
+        this.original = original;
+    }
+
+    @Override
+    public String toString() {
+        return "TextMessage{" +
+                "sender='" + this.sender + '\'' +
+                "action='" + this.action + '\'' +
+                "to original message='" + this.original + '\'' +
+                '}';
+    }
 }
