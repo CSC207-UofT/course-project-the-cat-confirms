efa30cfe1b769ebac9abaad03a7226f0246a9b32
yifan <peteryifan.zhao@mail.utoronto.ca>
2021-12-05 18:02:11 -0500

    phase2 ChatroomManagerTest in progress.

--- src/test/java/ChatroomManagerTest.java
+++ src/test/java/ChatroomManagerTest.java
@@ -8,13 +8,13 @@ import org.json.simple.JSONValue;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.*;
 
-//public class ChatroomManagerTest {
-//    @Test
-//    public void TestAddChatRoom(){
-//        User peter = new User("Peter");
-//        Chatroom chatroom1 = new Chatroom("cat_confirm", peter);
-//        ChatroomManager chatroomManager = new ChatroomManager(peter);
-//        chatroomManager.addChatRoom(chatroom1.getRoomName(), peter, chatroom1.getRoomId());
-//        Assertions.assertTrue(chatroomManager.);
-//    }
-//}
+public class ChatroomManagerTest {
+    @Test
+    public void TestAddChatRoom(){
+        User peter = new User("Peter");
+        Chatroom chatroom1 = new Chatroom("cat_confirm", peter);
+        ChatroomManager chatroomManager = new ChatroomManager(peter);
+        chatroomManager.addChatRoom(chatroom1.getRoomName(), peter, chatroom1.getRoomId());
+        Assertions.assertTrue(chatroomManager.);
+    }
+}
