65184db9b9e22dffa7e156461c73b722059d3b70
yifan <peteryifan.zhao@mail.utoronto.ca>
2021-12-06 01:50:54 -0500

    phase2 update the factory pattern for Message

--- src/test/java/MessageTest.java
+++ src/test/java/MessageTest.java
@@ -16,6 +16,7 @@ public class MessageTest {
 
         TextMessage tmsg = new TextMessage("Ice cream is awesome, like if you agree!", owner);
         Message msg2 = Message.messageMaker(":)", owner, tmsg);
+        Message msg3 = Message.messageMaker("Dislike", owner, tmsg);
         ActionMessage amsg = new ActionMessage("Like", owner, tmsg);
         File sourceimage = new File("test.png");
         ImageMessage imsg = new ImageMessage(sourceimage, owner);
@@ -23,10 +24,12 @@ public class MessageTest {
         String result2 = imsg.toString();
         String result3 = msg.toString();
         String result4 = msg2.toString();
+        String resilt5 = msg3.toString();
 
         Assertions.assertEquals("ActionMessage{sender='Varun'action='Like'the original message='TextMessage{sender='Varun'msg='Ice cream is awesome, like if you agree!'}'}", result1);
         Assertions.assertEquals("ImageMessage{sender='Varun'image='test.png'}",result2);
         Assertions.assertEquals("TextMessage{sender='Varun'msg='123'}",result3);
         Assertions.assertEquals("ActionMessage{sender='Varun'action=':)'the original message='TextMessage{sender='Varun'msg='Ice cream is awesome, like if you agree!'}'}", result4);
+        Assertions.assertEquals("ActionMessage{sender='Varun'action='Dislike'the original message='TextMessage{sender='Varun'msg='Ice cream is awesome, like if you agree!'}'}",resilt5);
     }
 }
