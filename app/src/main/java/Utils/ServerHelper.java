e466e5bfffc18ae487e82f3f307fbaf8eec3e4e7
Junhao Liao <junhao@junhao.ca>
2021-12-08 04:18:23 -0500

    [Backend] refactor Server code

--- app/src/main/java/Utils/ServerHelper.java
+++ app/src/main/java/Utils/ServerHelper.java
@@ -4,10 +4,11 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ServerParser {
+public class ServerHelper {
     // https://www.rgagnon.com/javadetails/java-get-url-parameters-using-jdk-http-server.html
     public static Map<String, String> queryToMap(String query) {
         Map<String, String> result = new HashMap<String, String>();
@@ -30,5 +31,13 @@ public class ServerParser {
             msgStringBuilder.append((char) i);
         }
         return msgStringBuilder.toString();
+    }
+
+    static public void sendHTTPResponse(HttpExchange t, String response) throws IOException{
+        t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
